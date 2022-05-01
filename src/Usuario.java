fe3df0b722eec18b314603067d4be1b033cc7ed4
PedroCarlucci <pkcarlucci@gmail.com>
2022-05-01 18:32:39 -0300

    adicionando classe usuario e seu construtor

---
+++ src/Usuario.java
@@ -0,0 +1,12 @@
+public class Usuario {
+
+    private int identificao;
+    private String nome;
+    private String funcao;
+
+    public Usuario(int identificao, String nome, String funcao) {
+        this.identificao = identificao;
+        this.nome = nome;
+        this.funcao = funcao;
+    }
+}
