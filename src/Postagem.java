1ee0b07901fb7bd7478fb9b8fd37209854ec0999
Felipe Cardona <feliperavacardona@hotmail.com>
2022-05-01 19:18:41 -0300

    criacao da classe postagem e alguns metodos

--- src/Postagem.java
+++ src/Postagem.java
@@ -1,4 +1,50 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class Postagem
 {
-    
+    private Usuario usuario;
+    private String data;
+    private String texto;
+    SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+    public Postagem(Usuario usuario, String texto)
+    {
+        this.usuario = usuario;
+        this.data = formato.format(new Date());
+        this.texto = texto;
+    }
+
+    @Override
+    public String toString() {
+        return "Postagem{" +
+                "usuario=" + usuario +
+                ", data='" + data + '\'' +
+                ", texto='" + texto + '\'' +
+                '}';
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 }
