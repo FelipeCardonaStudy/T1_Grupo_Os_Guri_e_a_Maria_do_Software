6110704e4814b8b6e10c2cd7ab11d62631a6e153
l-ehara <l.ehara@edu.pucrs.br>
2022-05-10 14:58:13 -0300

    Criacao de postagens e usuarios

--- src/Dados.java
+++ src/Dados.java
@@ -1,9 +1,36 @@
+import java.util.ArrayList;
+
 public class Dados {
-    
+
     private final int MAX_SIZE_USERS = 10;
     private final int MAX_SIZE_POSTS = 100;
     private Usuario[] user = new Usuario[MAX_SIZE_USERS];
     private Postagem[] post = new Postagem[MAX_SIZE_POSTS];
+
+
+    ArrayList<Usuario> usuarios = new ArrayList<>(); //Arraylist de usuários.
+    ArrayList<Postagem> pnao = new ArrayList<>(); //Arraylist de postagens não autorizadas.
+    ArrayList<Postagem> psim = new ArrayList<>(); //Arraylist de postagens autorizadas.
+
+
+    public void inicializa(){
+
+        Usuario admin = new Usuario(1, "Fulano", Usuario.FuncaoUsuario.Administrador);
+        Usuario user1 = new Usuario(2,"Ciclano", Usuario.FuncaoUsuario.Funcionario);
+        Usuario user2 = new Usuario(3,"Robson", Usuario.FuncaoUsuario.Funcionario);
+        Usuario user3 = new Usuario(4,"Jaqueline", Usuario.FuncaoUsuario.Funcionario);
+
+
+        Postagem p1 = new Postagem(user1,"Eu gosto de bananas!", Postagem.TagsPostagem.Comida);
+        Postagem p2 = new Postagem(user1,"Vou adotar um papagaio.", Postagem.TagsPostagem.Animais);
+        Postagem p3 = new Postagem(user2,"Vou viajar para a Itália amanhã!!", Postagem.TagsPostagem.Viagem);
+        Postagem p4 = new Postagem(user2,"Lagoa Azul é meu filme preferido!", Postagem.TagsPostagem.Filmes);
+        Postagem p5 = new Postagem(user3,"Alguém quer jogar basquete amanhã??", Postagem.TagsPostagem.Esportes);
+        Postagem p6 = new Postagem(user3,"Hello World!! :)");
+        Postagem p7 = new Postagem(admin,"Amanhã será lançada uma atualização do aplicativo.");
+
+
+    }
 
 
     /**
@@ -29,5 +56,5 @@ public class Dados {
     public String getUserNameByIndex(int index) {
         return user[index].getNome();
     }
-    
+
 }
