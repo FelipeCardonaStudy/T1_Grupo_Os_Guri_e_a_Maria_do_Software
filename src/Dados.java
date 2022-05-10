c7eb605eb151ddff79a92e5af7552e50720d9e39
l-ehara <l.ehara@edu.pucrs.br>
2022-05-10 15:46:20 -0300

    criacao do metodo addPost e postagens

--- src/Dados.java
+++ src/Dados.java
@@ -21,15 +21,36 @@ public class Dados {
         Usuario user3 = new Usuario(4,"Jaqueline", Usuario.FuncaoUsuario.Funcionario);
 
 
-        Postagem p1 = new Postagem(user1,"Eu gosto de bananas!", Postagem.TagsPostagem.Comida);
+        Postagem p1 = new Postagem(user1,"Eu gosto de bananas!", Postagem.TagsPostagem.Comida); //Posts normais
         Postagem p2 = new Postagem(user1,"Vou adotar um papagaio.", Postagem.TagsPostagem.Animais);
-        Postagem p3 = new Postagem(user2,"Vou viajar para a Itália amanhã!!", Postagem.TagsPostagem.Viagem);
+        Postagem p3 = new Postagem(user2,"Vou viajar para a Italia amanha!!", Postagem.TagsPostagem.Viagem);
         Postagem p4 = new Postagem(user2,"Lagoa Azul é meu filme preferido!", Postagem.TagsPostagem.Filmes);
-        Postagem p5 = new Postagem(user3,"Alguém quer jogar basquete amanhã??", Postagem.TagsPostagem.Esportes);
+        Postagem p5 = new Postagem(user3,"Alguem quer jogar basquete amanha??", Postagem.TagsPostagem.Esportes);
         Postagem p6 = new Postagem(user3,"Hello World!! :)");
-        Postagem p7 = new Postagem(admin,"Amanhã será lançada uma atualização do aplicativo.");
 
+        Postagem p7 = new Postagem(admin,"Amanha será lançada uma atualizacao do aplicativo."); //Post admin
 
+        Postagem p8 = new Postagem(user1,"Que dia merda."); // Post a ser proibido
+
+        addPost(p1);
+        addPost(p2);
+        addPost(p3);
+        addPost(p4);
+        addPost(p5);
+        addPost(p6);
+        addPost(p7);
+
+        addPost(p8);
+    }
+
+    public boolean addPost(Postagem p){
+
+        if(p.getAutorizacao() == Postagem.Autorizacao.NaoAutorizada){
+            pnao.add(p);
+            return false;
+        }
+        psim.add(p);
+        return true;
     }
 
 
