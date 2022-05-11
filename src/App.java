684a20b7333cefc08b40242df029839b2e6df35e
PedroCarlucci <pkcarlucci@gmail.com>
2022-05-10 21:12:56 -0300

    adicionando git ignore para classes e modificando switch

--- src/App.java
+++ src/App.java
@@ -47,11 +47,11 @@ public class App {
 
             while (acao <= 5) {
                 switch (acao) {
-                    case 1 -> escolheUsuarioAtivo();
-                    case 2 -> listaPostagens();
-                    case 3 -> excluiPostagem();
-                    case 4 -> pesquisaPostagem();
-                    case 5 -> salvaPostagensCSV();
+                    case 1: escolheUsuarioAtivo();
+                    case 2: listaPostagens();
+                    case 3: excluiPostagem();
+                    case 4: pesquisaPostagem();
+                    case 5: salvaPostagensCSV();
                 }
                 acao = 7; // mudar o valor da variavel acao para retornar para o loop do menu
             }
